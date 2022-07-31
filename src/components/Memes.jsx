b56e416df556134735b5a1e344c408e78969cab5
madvier83 <madvier83@gmail.com>
2022-07-31 16:47:45 +0700

    Save as PNG update

--- src/components/Memes.jsx
+++ src/components/Memes.jsx
@@ -1,4 +1,21 @@
-import React, {useEffect, useState} from "react"
+import React, {forwardRef, useRef, useEffect, useState} from "react"
+import { exportComponentAsPNG } from 'react-component-export-image'
+
+const ComponentToPrint = forwardRef((props, ref) => {
+    const formData = props.formData
+    const currentMeme = props.currentMeme
+
+    return (
+        <div className="relative max-w-sm mx-auto text-white font-extrabold overflow-hidden" ref={ref}>
+            <img src={currentMeme?.url} alt="" className="-z-10" />
+            <h1 className="text-2xl sm:text-3xl md:text-4xl text-center absolute top-2 w-full text-border">
+                {formData.topText}
+            </h1>
+            <h1 className="text-2xl sm:text-3xl md:text-4xl text-center absolute bottom-2 w-full text-border">
+                {formData.bottomText}
+            </h1>
+        </div>
+  )});
 
 export default function Memes() {
 
@@ -30,11 +47,12 @@ export default function Memes() {
             }
         })
     }
-    console.log(currentMeme)
+
+    const componentRef = useRef()
 
     return (
         <>
-            <div className="flex mt-16">
+            <div className="flex mt-8 md:mt-12">
                 <form className="flex flex-col container mx-auto max-w-3xl">
                     <div className="flex">
                         <input 
@@ -54,19 +72,21 @@ export default function Memes() {
                             onChange={handleChange}
                             />
                     </div>
-                    <button type="button" onClick={randomMeme} className="btn btn-primary m-2">Get New Meme Image</button>
+                    <button type="button" onClick={randomMeme} className="btn btn-primary m-2">
+                        Get New Meme Image
+                    </button>
                 </form>
             </div>
 
-            <div className="relative max-w-sm mx-auto text-white font-extrabold">
-                <img src={currentMeme?.url} alt="" className="-z-10" />
-                <h1 className="text-4xl text-center absolute top-2 w-full text-border">
-                    {formData.topText}
-                </h1>
-                <h1 className="text-4xl text-center absolute bottom-2 w-full text-border">
-                    {formData.bottomText}
-                </h1>
-            </div>
+            <ComponentToPrint ref={componentRef} formData={formData} currentMeme={currentMeme}/>
+            {currentMeme?.url && (
+                <div className="flex mx-2 md:mx-4">
+                    <button onClick={() => exportComponentAsPNG(componentRef)} className="btn btn-success m-2 mx-auto w-full max-w-3xl">
+                        Save As PNG
+                    </button>
+                </div>
+                )
+            }
         </>
     )
 }
